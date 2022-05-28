2043b0bab25b8f9cc7bedf3c4d9f0c92ac7ebec4
wiley-porg <willychan2022@gmail.com>
2022-05-28 12:16:42 -0400

    comments and cleaned up midpoint karel

--- MidpointKarel.py
+++ MidpointKarel.py
@@ -15,51 +15,94 @@ assume that it is at least as tall as it is wide.
 
 def main():
     """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
+    To place exactly one beeper at the midpoint of the lowest row of the environment.
+    If there are an even number of columns in the environment, Karel can place a single beeper
+    on either of the two center-most spaces.
     """
 
-
-
-    while no_beepers_present():
-        check_move()
-        if beepers_present():
-            reverse()
-            check_move()
-            put_beeper()
-            check_move()
-
-        if front_is_blocked():
-            put_beeper()
-            reverse()
-            check_move()
-
+    create_boundary()
+    place_an_extra_beeper_on_the_midpoint()
+    move_to_wall()
     reverse()
-    check_move()
-    put_beeper()
+    pick_up_every_beeper()
 
+
+def move_to_wall():
+    """
+        Karel will move forward until she reaches a wall.
+    """
     while front_is_clear():
-        check_move()
-    reverse()
+        safe_move()
+
+def pick_up_every_beeper():
+    """
+    precondition: Karel is at the rightmost or leftmost point, and is facing west or east respectively.
+    postcondition: Karel is at the opposite position to which she started in the same orientation.
+
+    If Karel detects a beeper present, she picks it up and moves forward: checking whether there is a
+    beeper on the next block.
+    """
     while beepers_present():
         pick_beeper()
-        check_move()
+        safe_move()
+
+def place_an_extra_beeper_on_the_midpoint():
+    """
+        precondition: Karel is on the space directly to the left of the midpoint.
+        postcondition: Karel is at the midpoint, which now has 2 beepers placed on it.
+    """
+    reverse()
+    safe_move()                 # Fence-post problem: will always initially "overshoot" the
+    put_beeper()                # midpoint due to constrict_boundary's safe_move after putting a beeper down.
 
 
+def create_boundary():
+    """
+        precondition: Karel is facing east at (1,1).
+        postcondition: Karel is on the space directly to the left of the midpoint, having placed
+                        beepers on every space on the lowest row.
+    """
+    while no_beepers_present():
+        safe_move()
+        if beepers_present():           # This if-statement MUST come first, since
+            constrict_boundary()        # the presence of a beeper must overshadow
+        if front_is_blocked():          # the presence of a wall.
+            set_initial_boundary()
 
 
+def set_initial_boundary():
+    """
+        precondition: Karel is at the rightmost column & lowest row facing east.
+        postcondition: Karel is at the (empty) space left to the rightmost column & lowest row
+    """
+    put_beeper()
+    reverse()
+    safe_move()
 
-
+def constrict_boundary():
+    """
+        precondition: Karel is on a space containing a beeper.
+        postcondition: Karel is 2 spaces from her initial position, having placed 1 beeper adjacent
+                        to her initial position.
+    """
+    reverse()
+    safe_move()
+    put_beeper()
+    safe_move()
 
 
 
 def reverse():
+    """
+       Karel makes a 180-degree turn in place.
+    """
     turn_left()
     turn_left()
 
-def check_move():
+def safe_move():
+    """
+    Karel moves forward if there is no wall in front of her.
+    """
     if front_is_clear():
         move()
 
